6f3184639ab468be36fc0057f3c147209f1750d5
justinzeng <zengjustin@126.com>
2019-07-15 16:11:28 +0800

    Update MyBatisTest.java
    2019/7/15 Update.

--- MyBatis/src/com/mybatis/test/MyBatisTest.java
+++ MyBatis/src/com/mybatis/test/MyBatisTest.java
@@ -3,8 +3,8 @@ package com.mybatis.test;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
-
 import org.apache.ibatis.session.SqlSession;
 import com.mybatis.datasource.DataConnection;
 import com.mybatis.po.User;
@@ -23,9 +23,11 @@ public class MyBatisTest {
 		result.append("省份: " + user.getProvince() + "\r\n");
 		result.append("城市: " + user.getCity() + "\r\n");
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		result.append("出生日期: " + sdf.format(user.getbirthday()) + "\r\n");
+		result.append("出生日期: " + sdf.format(user.getBirthday()) + "\r\n");
+		result.append("年龄：" + user.getAge() + "\r\n");
 		
 		System.out.println(result.toString());
+
 		sqlSession.close();
 	}
 	
@@ -40,7 +42,7 @@ public class MyBatisTest {
 		SqlSession sqlSession = dataConn.getSqlSession();
 		User user = new User();
 		user.setId(7);
-		user.setGender("女");
+		user.setGender("男");
 		
 		sqlSession.update("SQLtest.updateUser", user);
 		sqlSession.commit();
@@ -49,16 +51,18 @@ public class MyBatisTest {
 	
 	public void TestInsert() throws IOException, ParseException {
 		SqlSession sqlSession = dataConn.getSqlSession();
-		User user = new User();
-		user.setUsername("杨丽");
-		user.setPassword("666");
-		user.setGender("女");
-		user.setEmail("666@126.com");
-		user.setProvince("广东省");
-		user.setCity("广州市");
+
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		user.setbirthday(sdf.parse("1999-03-18"));
+		String userBirthday = "1999-03-18";
+		Date uBirthday = sdf.parse(userBirthday);
+		User user = new User("杨丽","666","女","666@126.com","广东省","广州市",uBirthday);
+//		user.setBirthday(sdf.parse(userBirthday));
 		
+		// 计算年龄
+		Date Now = new Date();
+		long age = (((Now.getTime() - uBirthday.getTime()) / (24*60*60*1000)) / 365);
+		user.setAge((int)age);
+
 		sqlSession.insert("SQLtest.insertUser", user);
 		sqlSession.commit();
 		sqlSession.close();
@@ -78,7 +82,8 @@ public class MyBatisTest {
 			result.append("省份: " + user.getProvince() + "\r\n");
 			result.append("城市: " + user.getCity() + "\r\n");
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-			result.append("出生日期: " + sdf.format(user.getbirthday()) + "\r\n");
+			result.append("出生日期: " + sdf.format(user.getBirthday()) + "\r\n");
+			result.append("年龄: " + user.getAge() + "\r\n");
 			
 			System.out.println(result.toString());
 			result.setLength(0);
